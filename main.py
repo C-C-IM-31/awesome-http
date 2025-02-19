6cb9675458a34d62734cbcfbd7e80539f3f57047
Lytvynenko Serhii <serrheylitvinenko@gmail.com>
2025-02-19 23:54:36 +0200

    basics

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
-def main():
-    print("Hello world")
+import asyncio
+import json
+from asyncio import StreamReader, StreamWriter
+from datetime import datetime, timezone
+
+host = "127.0.0.1"
+port = 8000
+
+
+async def handler(reader: StreamReader, writer: StreamWriter):
+    time = datetime.now(timezone.utc).isoformat()
+    response = json.dumps({"time": time})
+    http_header = (
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: text/plain\r\n"
+        f"Content-Length: {len(response)}\r\n"
+        "\r\n"
+    )
+    packet = http_header + response
+    writer.write(packet.encode())
+    await writer.drain()
+    writer.close()
+    await writer.wait_closed()
+
+
+async def main():
+    server = await asyncio.start_server(handler, host, port)
+    async with server:
+        await server.serve_forever()
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
